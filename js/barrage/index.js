70815cbebfce65acdc8b244b2fab0014d3f01a33
WarmAirBlowing <931385178@qq.com>
2020-11-06 19:29:04 +0800

    实现视频弹幕功能

---
+++ js/barrage/index.js
@@ -0,0 +1,151 @@
+let data = [
+    { value: '麻烦来给我的爱人来一杯mojito',time: 5,color: 'red',speed: 1,fontSize: 22},
+    { value: '周杰伦真帅',time: 10,color: '00alf5',speed: 1,fontSize: 30},
+    { value: '我喜欢你',time: 14,color: '#00alf5',speed: 1},
+    { value: '我的咖啡糖不用太多',time: 20},
+]
+
+let canvas = document.getElementById('canvas')
+let video = document.getElementById('video')
+let $txt = document.getElementById('text')
+let $btn = document.getElementById('btn')
+let $color = document.getElementById('color')
+let $range = document.getElementById('range')
+
+// 创建渲染弹幕的类
+class CanvasBarrage {
+    // opts = {} 表示参数的默认值
+    constructor(canvas,video,opts = {}) {
+        if (!canvas || !video) return
+
+        // 将传进来的参数挂载在 this 上
+        this.video = video
+        this.canvas = canvas
+        // 设置 canvas 的宽高和 video 宽高一致
+        this.canvas.width = video.width
+        this.canvas.height = video.height
+
+        // 获取画布，操作画布
+        this.ctx = canvas.getContext('2d')
+
+        // 设置默认参数，如果用户没传我就给它带上
+        let defOpts = {
+            color: '#191e63',
+            speed: 1.5,
+            opacity: 0.5,
+            fontSize: 20,
+            data: []
+        }
+        // 合并对象 再挂载到 this 上
+        Object.assign(this,defOpts,opts) 
+        // 添加属性，用来判断播放暂停，默认 true 是暂停
+        this.isPaused = true
+        // 得到所有的弹幕消息
+        this.barrages = this.data.map(item => new Barrage(item,this)) // 交给 map 操作
+        // 渲染弹幕
+
+    }
+    // 渲染 canvas 绘制的弹幕
+    render() {
+        // 绘制的第一步，清除原来的画布
+        this.clear()
+        // 渲染
+        this.renderBarrage()
+        //
+        if(this.isPaused === false) {
+            // 新增的定时器 根据屏幕刷新率来执行
+            requestAnimationFrame(this.render.bind(this))
+        }
+    }
+    clear() {
+        // 清除画布 canvas自带的方法
+        this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height)
+    }
+    renderBarrage() {
+        // 首先拿到当前视频播放的时间
+        let time = this.video.currentTime
+        this.barrages.forEach(barrage => {
+            if(time >= barrage.time && !barrage.flag) {
+                // 如果弹幕没有初始化完成，就先初始化
+                if(!barrage.isInit) { 
+                    barrage.init()
+                    barrage.isInit = true
+                }
+
+                // 弹幕从右往左渲染
+                barrage.x -= barrage.speed
+                barrage.renderOnce() // 渲染每一条弹幕
+                // 判断是否走出画布
+                if(barrage.x < -barrage.width) {
+                    barrage.flag = true
+                }
+            }
+        })
+    }
+    // 接收新的弹幕
+    add(obj) {
+        this.barrages.push(new Barrage(obj,this))
+    }
+}
+
+// 初始化弹幕的类
+// 让每一条弹幕里面的属性都能生效
+class Barrage {
+    constructor(obj,ctx) {
+        this.value = obj.value // 弹幕的内容
+        this.time = obj.time // 弹幕出现的时间
+        this.obj = obj
+        this.context = ctx
+    }
+    // 初始化弹幕
+    init() {
+        this.color = this.obj.color || this.context.color // 优先取前面的值，没有的话取后面的
+        this.speed = this.obj.speed || this.context.speed
+        this.opacity = this.obj.opacity || this.context.opacity
+        this.fontSize = this.obj.fontSize || this.context.fontSize
+
+        // 计算每条弹幕的宽度
+        let p = document.createElement('p')
+        p.style.fontSize = this.fontSize + 'px'
+        p.innerHTML = this.value
+        document.body.appendChild(p)
+        this.width = p.clientWidth // 拿到 p 标签的宽度
+        document.body.removeChild(p) // 取到宽度后就移除，没什么用了
+
+        // 设置弹幕出现的位置
+        this.x = this.context.canvas.width
+        this.y = this.context.canvas.height * Math.random()
+        // 超出范围处理
+        if(this.y < this.fontSize) {
+            this.y = this.fontSize
+        }else if(this.y > this.context.canvas.height - this.fontSize) {
+            this.y = this.context.canvas.height - this.fontSize
+        }
+    }
+
+    // 渲染每一条弹幕
+    renderOnce() {
+        this.context.ctx.font = `${this.fontSize}px Arial`
+        this.context.ctx.fillStyle = this.color
+        // 绘制文字
+        this.context.ctx.fillText(this.value,this.x,this.y)
+    }
+}
+
+let canvasBarrage = new CanvasBarrage(canvas,video,{data})
+video.addEventListener('play', () => {
+    canvasBarrage.isPaused = false
+    canvasBarrage.render()
+})
+
+// 发弹幕
+$btn.addEventListener('click',send)
+function send() {
+    let value = $txt.value
+    let time = video.currentTime
+    let color = $color.value
+    let fontSize = $range.value
+    let obj = {value,time,color,fontSize}
+    canvasBarrage.add(obj)
+    $txt.value = ''
+}

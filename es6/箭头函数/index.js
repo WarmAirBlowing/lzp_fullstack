129fbe0fcb5f6c3e3883092ad74682300f353b82
WarmAirBlowing <931385178@qq.com>
2020-11-21 13:36:13 +0800

    箭头函数

---
+++ es6/箭头函数/index.js
@@ -0,0 +1,14 @@
+// let foo = {
+//     value: 1,
+//     getValue: () => console.log(this.value) // 箭头函数没有this
+// }
+
+// foo.getValue() // undefined
+
+// function Foo() {
+//     this.value = 1
+// }
+// Foo.prototype.getValue = () => console.log(this.value);
+// let foo = new Foo()
+// foo.getValue() // undefined
+

8f83e75f21e8d82e744c0b20a39cd1da1c39bdd2
WarmAirBlowing <931385178@qq.com>
2020-11-21 13:38:06 +0800

    es6语法

---
+++ es6/index.js
@@ -0,0 +1,12 @@
+// var foo = 'bar'
+// console.log(foo);
+
+// let foo = 'bar'
+
+// const foo = 'bar'
+// foo = 123
+// console.log(foo);
+
+// let example = 'Tom'
+// const foo = 'this is ' + example
+// const foo = `this is ${example}`

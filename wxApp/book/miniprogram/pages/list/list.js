7b14b42be6ec10ddf13c571f3d317ac6324412e2
WarmAirBlowing <931385178@qq.com>
2020-11-22 23:36:33 +0800

    书架页修改

--- wxApp/book/miniprogram/pages/list/list.js
+++ wxApp/book/miniprogram/pages/list/list.js
@@ -18,46 +18,85 @@ Page({
     }).get().then(res => {
       // console.log(res);
       wx.hideLoading()
-      let data = res.data
+      let data = res.data || []
       this.setData({
         bookItem: data
       })
+    }).catch(err => {
+      wx.hideLoading()
+      wx.showToast({
+        icon: 'none',
+        title: '网络异常，稍后重试'
+      }, 3000)
     })
   },
   navTo(e) {
     // console.log(e);
-    let url = e.currentTarget.dataset.url
-    let name = e.currentTarget.dataset.bookName
-    let imgUrl = e.currentTarget.dataset.imgurl
-    wx.navigateTo({
-      url: `../bookContent/bookContent?url=${url}&name=${name}&imgUrl=${imgUrl}`,
-    })
+    let url = e.currentTarget.dataset.url || ''
+    let name = e.currentTarget.dataset.bookName || ''
+    let imgUrl = e.currentTarget.dataset.imgurl || ''
+    if (url) {
+      wx.navigateTo({
+        url: `../bookContent/bookContent?url=${url}&name=${name}&imgUrl=${imgUrl}`,
+      })
+    } else {
+      wx.showToast({
+        title: '数据不存在',
+      }, 3000)
+    }
+
   },
   delete(e) {
-    // console.log(e);
-    let name = e.currentTarget.dataset.bookName
-    db.collection('book').where({
-      _openid: app.globalData.openid,
-      bookName: name
-    }).get().then(res => {
-      console.log(res);
-      let data = res.data
-      const id = data[0]._id
-      db.collection('book').doc(id).remove({
-        success: function(res) {
-          wx.showToast({
-            title: '删除成功',
+    let that = this;
+    let name = e.currentTarget.dataset.name || '';
+    wx.showModal({
+      title: '提示',
+      content: `确定要删除${name}吗？`,
+      success: function (res) {
+        if (res.confirm) {
+          console.log('点击确定了');
+          db.collection('book').where({
+            _openid: app.globalData.openid,
+            bookName: name
+          }).get().then(res => {
+            // console.log(res)
+            const data = res.data || [];
+            if (data.length > 0) {
+              db.collection('book').doc(data[0]._id).remove().then(res => {
+                console.log(res);
+                if (res.stats.removed > 0) {
+                  wx.showToast({
+                    icon: 'success',
+                    title: '删除成功',
+                    success: (res) => {
+                      console.log(res);
+                      that.getBook();
+                    }
+                  }, 3000);
+                }
+              }).catch(err => {
+                wx.showToast({
+                  icon: 'none',
+                  title: '删除失败',
+                  success: function (res) {
+                  }
+                }, 3000)
+              })
+            }
+          }).catch(err => {
           })
-          this.getBook()
+        } else if (res.cancel) {
+          console.log('点击取消了');
+          return false;
         }
-      })
+      }
     })
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**

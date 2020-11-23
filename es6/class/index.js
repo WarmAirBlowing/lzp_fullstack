0f17f077f7114e66ec174a65430f70d9da6384db
WarmAirBlowing <931385178@qq.com>
2020-11-23 21:20:43 +0800

    class

---
+++ es6/class/index.js
@@ -0,0 +1,31 @@
+// class Foo {
+//     constructor() {
+
+//     }
+//     static bar() {
+//         this.baz()
+//     }
+//     static baz() {
+//         console.log('hello');
+//     }
+//     // baz() {
+//     //     console.log('world');
+//     // }
+// }
+// let foo = new Foo()
+
+// foo.bar() // 报错
+
+class Shape {
+    constructor(width,height) {
+        this._width = width
+        this._height = height
+        // this.area = width * height
+    }
+    get area() {
+        return this._width * this._height
+    }
+}
+const square = new Shape(10,10) 
+console.log(square._width);
+console.log(square.area);

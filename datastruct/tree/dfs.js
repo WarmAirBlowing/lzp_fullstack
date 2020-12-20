5e6734115674090ca527fa9a2c40411bf7aeeeb8
WarmAirBlowing <931385178@qq.com>
2020-12-20 23:52:26 +0800

    深度优先遍历

---
+++ datastruct/tree/dfs.js
@@ -0,0 +1,25 @@
+// 树的深度优先遍历
+// 1. 访问根节点
+// 2. 对根节点 children 依次进行深度优先遍历
+let root = {
+  val: 1,
+  left: {
+    val: 2,
+    left: {val: 3},
+    right: {val: 4}
+  },
+  right: {
+    val: 3,
+    left: {val: 5},
+    right: {val: 6}
+  }
+}
+
+let dfs = (n) => {
+  if(!n) return;
+  console.log(n.val);
+  dfs(n.left);
+  dfs(n.right);
+}
+
+dfs(root)

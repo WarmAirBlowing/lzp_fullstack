389c35eae82436fa32908dff478acdcb4453f558
WarmAirBlowing <931385178@qq.com>
2020-12-21 23:57:26 +0800

    柯里化

---
+++ js/柯里化/curry.js
@@ -0,0 +1,43 @@
+// function curry(fn) {
+//   let args = [].slice.call(arguments, 1) // 拿到fn的参数
+//   return function () {
+//     let newArgs = args.concat([].slice.call(arguments))
+//     return fn.apply(this, newArgs)
+//   }
+// }
+
+// function add(a, b) {
+//   return a + b
+// }
+// let addCurry = curry(add, 1, 2)
+// console.log(addCurry());
+
+
+
+function sub_curry(fn) {
+  let args = [].slice.call(arguments, 1)
+  return function () {
+    let newArgs = args.concat([].slice.call(arguments))
+    return fn.apply(this, newArgs)
+  }
+}
+
+function curry(fn, length) {
+  length = length || fn.length; // 函数的 length 属性 表示参数个数
+  let slice = Array.prototype.slice;
+  return function () {
+    if (arguments.length < length) {
+      let combined = [fn].concat(slice.call(arguments))
+      return curry(sub_curry.apply(this, combined), length - arguments.length)
+    } else {
+      return fn.apply(this, arguments)
+    }
+  }
+}
+
+let test = curry(function (a, b, c) {
+  return [a, b, c]
+})
+
+console.log(test('a','b','c'));
+console.log(test('a','b')('c'));
---
+++ js/柯里化/simple.js
@@ -0,0 +1,23 @@
+function sub_curry(fn) {
+  return function() {
+    return fn()
+  }
+}
+
+function curry(fn, length) {
+  length = length || 4
+  return function() {
+    if(length > 1) {
+      return curry(sub_curry(fn), --length)
+    }else{
+      return fn()
+    }
+  }
+}
+
+function func() {
+  console.log(1);
+}
+let fn0 = curry(func)
+
+fn0()()()()

5cb5c46c9d68547813b3e20547cccad3e51019e6
WarmAirBlowing <931385178@qq.com>
2020-12-30 10:38:02 +0800

    路由实现原理

--- vue/路由实现/route/src/router/myVueRouter.js
+++ vue/路由实现/route/src/router/myVueRouter.js
@@ -1,26 +1,87 @@
 let Vue = null
-class VueRouter {
-
+class HistoryRoute {
+  constructor() {
+    this.current = null
+  }
 }
-VueRouter.install = function(v) {
+class VueRouter {
+  constructor(options) {
+    this.mode = options.mode || 'hash'
+    this.routes = options.routes || []
+    this.routesMap = this.createMap(this.routes)
+
+    this.history = new HistoryRoute() // 装当前路由的状态
+    this.init()
+  }
+  // 初始化
+  init() {
+    if (this.mode === 'hash') {
+      location.hash ? '' : location = '/'
+      window.addEventListener('load', () => {
+        this.history.current = location.hash.slice(1)
+      })
+      window.addEventListener('hashchange', () => {
+        this.history.current = location.hash.slice(1)
+      })
+    } else {
+      location.pathname ? '' : location.pathname = '/'
+      window.addEventListener('load', () => {
+        this.history.current = location.pathname
+      })
+      window.addEventListener('popstate', () => {
+        this.history.current = location.pathname
+      })
+    }
+  }
+  createMap(routes) {
+    return routes.reduce((pre, current) => {
+      pre[current.path] = current.component // { '/home': Home }
+      return pre
+    }, {})
+  }
+}
+VueRouter.install = function (v) {
   Vue = v
   Vue.mixin({
     beforeCreate() {
       if (this.$options && this.$options.router) { // 如果是根组件
-
+        this._root = this
+        this._router = this.$options.router
+        Vue.util.defineReactive(this, 'xxx', this._router.history)
+      } else { //如果是子组件
+        this._root = this.$parent && this.$parent._root
       }
+      Object.defineProperty(this, '$router', {
+        get() {
+          return this._root._router
+        }
+      })
+      Object.defineProperty(this, '$route', {
+        get() {
+          return this._root._router.history.current
+        }
+      }) 
+
+      
     }
   })
   Vue.component('router-link', {
+    props: {
+      to: String
+    },
     render(h) {
-      return h('a', {}, '首页')
+      let mode = this._self._root._router.mode
+      let to = mode === 'hash' ? '#' + this.to : this.to
+      return h('a', {attrs: {href: to}}, this.$slots.default)
     }
   }),
-  Vue.component('router-view', {
-    render(h) {
-      return h('h1', {}, '首页视图')
-    }
-  })
+    Vue.component('router-view', {
+      render(h) {
+       let current = this._self._root._router.history.current
+       let routeMap = this._self._root._router.routesMap
+       return h(routeMap[current])
+      }
+    })
 }
 
 export default VueRouter

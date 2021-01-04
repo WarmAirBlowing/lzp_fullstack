73f63d36c75affe31a6dedde09e26a35e6568972
WarmAirBlowing <931385178@qq.com>
2021-01-04 23:41:06 +0800

    冒泡排序

---
+++ datastruct/sort/bubbleSort.js
@@ -0,0 +1,15 @@
+Array.prototype.bubbleSort = function () {
+  for (let i = 0; i < this.length - 1; i++) {
+    for (let j = 0; j < this.length - 1 - i; j++) {
+      if (this[j] > this[j + 1]) {
+        const temp = this[j];
+        this[j] = this[j + 1];
+        this[j + 1] = temp;
+      }
+    }
+  }
+  return this;
+}
+
+let arr = [2, 4, 1, 6, 3, 5];
+console.log(arr.bubbleSort());

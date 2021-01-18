40c3c75ae5eba6b9f0e5f041ca353ce72c959bac
WarmAirBlowing <931385178@qq.com>
2021-01-18 13:43:56 +0800

    购物车页面完成

--- vue/vue3/new-bee/src/views/Cart.vue
+++ vue/vue3/new-bee/src/views/Cart.vue
@@ -12,12 +12,19 @@
             </div>
             <div class="good-desc">
               <div class="good-title">
-                <span>{{item.goodsName}}</span>
-                <span>x{{item.goodsCount}}</span>
+                <span>{{ item.goodsName }}</span>
+                <span>x{{ item.goodsCount }}</span>
               </div>
               <div class="good-btn">
-                <div class="price">{{item.sellingPrice}}</div>
-                <van-stepper integer min="1" max="5" :model-value="item.goodsCount" :name="item.cartItemId" @change="numChange"/>
+                <div class="price">{{ item.sellingPrice }}</div>
+                <van-stepper
+                  integer
+                  min="1"
+                  max="5"
+                  :model-value="item.goodsCount"
+                  :name="item.cartItemId"
+                  @change="numChange"
+                />
               </div>
             </div>
           </div>
@@ -34,14 +41,27 @@
       </van-checkbox-group>
     </div>
     <!-- 合计 -->
-    <van-submit-bar v-if="true" class="submit-all" :price="total*100" button-text="结算" @submit="onSubmit">
-      <van-checkbox @click="allCheck" v-model:checked="checkAll">全选</van-checkbox>
-     
+    <van-submit-bar
+      v-if="list.length > 0"
+      class="submit-all"
+      :price="total * 100"
+      button-text="结算"
+      @submit="onSubmit"
+    >
+      <van-checkbox @click="allCheck" v-model:checked="checkAll"
+        >全选</van-checkbox
+      >
     </van-submit-bar>
-    <div class="empty" v-if="false">
-      <img class="empty-cart" src="//s.yezgea02.com/1604028375097/empty-car.png" alt="空购物车">
+    <div class="empty" v-else>
+      <img
+        class="empty-cart"
+        src="//s.yezgea02.com/1604028375097/empty-car.png"
+        alt="空购物车"
+      />
       <div class="title">购物车空空如也</div>
-      <van-button round color="#1baeae" type="primary" @click="goTo" block>前往选购</van-button>
+      <van-button round color="#1baeae" type="primary" @click="goTo" block
+        >前往选购</van-button
+      >
     </div>
     <navBar />
   </div>
@@ -51,62 +71,65 @@
 import sHeader from "@/components/SimpleHeader";
 import navBar from "@/components/NavBar";
 import { computed, onMounted, reactive, toRefs } from "vue";
-import { Toast } from 'vant'
-import { getCart, modifyCart } from '@/service/cart'
-
+import { Toast } from "vant";
+import { getCart, modifyCart, deleteCartItem } from "@/service/cart";
+import { useRouter } from 'vue-router'
 export default {
   components: {
     sHeader,
-    navBar
+    navBar,
   },
   setup() {
+    const router = useRouter()
     const state = reactive({
       result: [],
       list: [],
-      checkAll: true
+      checkAll: true,
     });
 
     onMounted(() => {
-      init()
-    })
-    const init = async() => {
+      init();
+    });
+    const init = async () => {
       Toast.loading({
-        message: '加载中...',
+        message: "加载中...",
         forbidClick: true,
       });
-      const { data } = await getCart({ pageNumber: 1})
-      state.list = data
-      state.result = data.map(item => item.cartItemId)
-      console.log(state.result);
-      Toast.clear()
-    }
+      const { data } = await getCart({ pageNumber: 1 });
+      state.list = data;
+      state.result = data.map((item) => item.cartItemId);
+      // console.log(state.result);
+      Toast.clear();
+    };
     // 全选
     const allCheck = () => {
-      if(!state.checkAll) {
-        state.result = state.list.map(item => item.cartItemId)
+      if (!state.checkAll) {
+        state.result = state.list.map((item) => item.cartItemId);
       } else {
-        state.result = []
+        state.result = [];
       }
-      state.checkAll = !state.checkAll
-    }
+      state.checkAll = !state.checkAll;
+    };
     // 总价格
     const total = computed(() => {
-      let sum = 0
-      let _list = state.list.filter(item => state.result.includes(item.cartItemId))
-      _list.forEach(item => {
-        sum += item.goodsCount * item.sellingPrice
-      })
-      return sum
-    })
+      let sum = 0;
+      let _list = state.list.filter((item) =>
+        state.result.includes(item.cartItemId)
+      );
+      _list.forEach((item) => {
+        sum += item.goodsCount * item.sellingPrice;
+      });
+      return sum;
+    });
     // 单选
     const groupChange = (result) => {
       // console.log(result);
-      if(result.length === state.list.length) {
-        state.checkAll = true
+      if (result.length === state.list.length) {
+        state.checkAll = true;
       } else {
-        state.checkAll = false
+        state.checkAll = false;
       }
-    }
+    };
     // 增减数量
     const numChange = async (value, detail) => {
       console.log(value, detail);
@@ -133,14 +156,47 @@ export default {
     };
     // 删除商品
     const deleteGood = async (goodId) => {
+      const { resultCode } = await deleteCartItem(goodId);
+      // if (resultCode == 200) {
+      //   // state.list.forEach((item, index) => {
+      //   //   if (item.cartItemId == goodId) {
+      //   //     state.list.splice(index, 1);
+      //   //   }
+      //   //   // Toast.clear();
+      //   // });
+      //   state.list = state.list.filter((item) => {
+      //     return item.cartItemId !== goodId
+      //   })
+      // } else {
+      //   Toast({
+      //     message: "出错了",
+      //     forbidClick: true,
+      //   });
+      // }
+      init()
+    };
 
+    const goTo = () => {
+      router.push({path: '/home'})
+    }
+
+    // 结算
+    const onSubmit = () => {
+      if(state.result.length == 0) {
+        Toast.fail('请选择商品进行结算')
+      }
+      const params = JSON.stringify(state.result)
+      router.push({path: '/create-order', query: {cartItemId: params}})
     }
     return {
       ...toRefs(state),
       allCheck,
       total,
       groupChange,
-      numChange
+      numChange,
+      deleteGood,
+      goTo,
+      onSubmit
     };
   },
 };

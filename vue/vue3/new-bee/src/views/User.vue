e350d540eb9d029358c15b588e1168e166bf9596
WarmAirBlowing <931385178@qq.com>
2021-01-22 22:44:36 +0800

    更新

--- vue/vue3/new-bee/src/views/User.vue
+++ vue/vue3/new-bee/src/views/User.vue
@@ -4,7 +4,7 @@
     <div class="user-info">
       <div class="info">
         <img src="//s.yezgea02.com/1604040746310/aaaddd.png" />
-        <div c lass="user-desc">
+        <div class="user-desc">
           <span>昵称：{{ user.nickName }}</span>
           <span>登录名：{{ user.loginName }}</span>
           <span class="name">个性签名：{{ user.introduceSign }}</span>

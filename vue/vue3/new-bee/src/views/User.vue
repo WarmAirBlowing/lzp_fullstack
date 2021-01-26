1a6fe08eb9746fcd5cf103a4e4cba83d04e46b6e
WarmAirBlowing <931385178@qq.com>
2021-01-26 23:55:35 +0800

    更新

--- vue/vue3/new-bee/src/views/User.vue
+++ vue/vue3/new-bee/src/views/User.vue
@@ -14,7 +14,7 @@
     <ul class="user-list">
       <li class="van-hairline--bottom" @click="goTo('/order')">
         <span>我的订单</span>
-        <van-icon nam e="arrow" />
+        <van-icon name="arrow" />
       </li>
       <li class="van-hairline--bottom" @click="goTo('/setting')">
         <span>账号管理</span>

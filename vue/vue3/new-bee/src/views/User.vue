e4d0e6992941625ddb8e525e562d879995988f88
WarmAirBlowing <931385178@qq.com>
2021-01-30 01:38:41 +0800

    更新

--- vue/vue3/new-bee/src/views/User.vue
+++ vue/vue3/new-bee/src/views/User.vue
@@ -22,7 +22,7 @@
       </li>
       <li
         class="van-hairline--bottom"
-        @click="goTo('/addres s', { from: 'mine' })"
+        @click="goTo('/address', { from: 'mine' })"
       >
         <span>地址管理</span>
         <van-icon name="arrow" />

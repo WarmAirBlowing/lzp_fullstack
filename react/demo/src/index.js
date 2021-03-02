5320106ad33ebecac0822c6bd580e69ed5a66f22
WarmAirBlowing <931385178@qq.com>
2021-03-02 23:55:44 +0800

    react基础

--- react/demo/src/index.js
+++ react/demo/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 // import App from './components/App'
-import E at from './components/eat'
+import Eat from './components/eat'
 
 ReactDom.render(<Eat />, document.getElementById('root'))
